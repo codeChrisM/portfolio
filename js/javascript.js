088157f543c2076790d8c746a6662ab268f3030e
chris marlowe <chris@codechris.com>
2016-06-13 15:53:30 -0500

    new Working versio (6/13/2016)

--- js/javascript.js
+++ js/javascript.js
@@ -32,15 +32,35 @@ $(window).scroll(function(){
     	/*var opacity = (wScroll - $('.fadeOnProject').offset().top+ 400 ) / (wScroll/6)
     	$('.fadeOnProject').addClass('is-showingProject');
     	console.log("class added Project");*/
-    	console.log("project added");
+    	console.log("Projects added");
     }
 
-        if(wScroll > $('#aboutMe').offset().top-($(window).height()/ 1.2)) {
-    
-    	var opacity = (wScroll - $('.fadeOn').offset().top+ 400 ) / (wScroll/6)
-    	$('.fadeOn').addClass('is-showing');
-    	console.log("class added");;
+
+	if(wScroll > $('#knowledge').offset().top-($(window).height()/ 1.2)) {
+    	
+    	$('.fadeOnKnowledge').each(function(i){
+    		setTimeout(function(){
+    			$('.fadeOnKnowledge').eq(i).addClass('is-showingKnowledge');
+    		}, 300 * (i+2));
+    	})
+    	/*var opacity = (wScroll - $('.fadeOnProject').offset().top+ 400 ) / (wScroll/6)
+    	$('.fadeOnProject').addClass('is-showingProject');
+    	console.log("class added Project");*/
+    	console.log("knowledge added");
     }
+
+	if(wScroll > $('#experience').offset().top-($(window).height()/ 1.2)) {
+    	
+    	$('.fadeOnExperience').each(function(i){
+    		setTimeout(function(){
+    			$('.fadeOnExperience').eq(i).addClass('is-showingExperience');
+    		}, 100 * (i+2));
+    	})
+    	/*var opacity = (wScroll - $('.fadeOnProject').offset().top+ 400 ) / (wScroll/6)
+    	$('.fadeOnProject').addClass('is-showingProject');
+    	console.log("class added Project");*/
+    	console.log("experience added");
+	}
 
 });
 
