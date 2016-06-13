3c61df3f5711171ea02f8b98c00c1a9073f02c5e
chris marlowe <chris@codechris.com>
2016-06-13 13:35:18 -0500

    transations up to projects/ mobile nav bar fixed position / website can still nudge left and right

--- js/javascript.js
+++ js/javascript.js
@@ -1,12 +1,48 @@
+ $(document).ready(function($) {
+
+ 	$('.fadeOnIconRow').each(function(i){
+    		setTimeout(function(){
+    			$('.fadeOnIconRow').eq(i).addClass('is-showingIconRow');
+    		}, 100 * (i+2));
+        	console.log("document ready");
+    	});
+ 	
+
+ });   
+
+
 $(window).scroll(function(){
 
   var wScroll = $(this).scrollTop();
 
-
-    if(wScroll > $('.aboutMe').offset().top-($(window).height()/ 1.2)) {
-      console.log("test");
-    	
-    	$('.aboutMe').addClass('is-showing');
-
+    if(wScroll > $('#aboutMe').offset().top-($(window).height()/ 1.2)) {
+    
+    	var opacity = (wScroll - $('.fadeOn').offset().top+ 400 ) / (wScroll/6)
+    	$('.fadeOn').addClass('is-showing');
+    	console.log("class added");;
     }
+
+    if(wScroll > $('#Projects').offset().top-($(window).height()/ 1.2)) {
+    	
+    	$('.fadeOnProject').each(function(i){
+    		setTimeout(function(){
+    			$('.fadeOnProject').eq(i).addClass('is-showingProject');
+    		}, 300 * (i+2));
+    	})
+    	/*var opacity = (wScroll - $('.fadeOnProject').offset().top+ 400 ) / (wScroll/6)
+    	$('.fadeOnProject').addClass('is-showingProject');
+    	console.log("class added Project");*/
+    	console.log("project added");
+    }
+
+        if(wScroll > $('#aboutMe').offset().top-($(window).height()/ 1.2)) {
+    
+    	var opacity = (wScroll - $('.fadeOn').offset().top+ 400 ) / (wScroll/6)
+    	$('.fadeOn').addClass('is-showing');
+    	console.log("class added");;
+    }
+
 });
+
+
+
