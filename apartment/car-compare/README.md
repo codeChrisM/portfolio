022b4972f3941ca43d9b17054b385a2eaf6cba1e
chris marlowe <chris@codechris.com>
2016-07-01 05:21:25 -0500

    fixed web links, cleared up empty code area

--- apartment/car-compare/README.md
+++
@@ -1,7 +0,0 @@
-car comparison tool 
-
-
-
-this is ttry and compare car compartision charts that dotn exist yet
-
-:x
--- apartment/message.js
+++
@@ -1,4 +0,0 @@
-function a_message()
-{
-alert("instructions:   press step 1, then step 2, then step 3, then step 4");
-} 
